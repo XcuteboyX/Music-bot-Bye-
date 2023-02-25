6078f785bb53b2d65afd3c5e87797eae4a052488
CUTE_BOY <124450535+XcuteboyX@users.noreply.github.com>
2023-02-25 22:25:52 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -71,8 +71,6 @@ SUPPORT_GROUP = getenv(
     "SUPPORT_GROUP", None
 )  # Example:- https://t.me/YukkiSupport
 
-SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
-
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
 
